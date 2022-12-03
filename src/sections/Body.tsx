11d314d23da3d11d2754d2e28c0d40eb5f64ca06
ry-se <ryans1633@gmail.com>
2022-12-03 13:49:58 +0800

    adding body text

--- src/sections/Body.tsx
+++ src/sections/Body.tsx
@@ -1,15 +1,24 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { motion } from 'framer-motion';
 
 const Body: React.FC = () => {
   return (
-    <div>
-        <div>
+    <Box>
+        <Box>
             <motion.div
             animate='animate'>
                 <h1> — WAIT, WHAT THE HACK IS A HACKATHON?</h1>
             </motion.div>
-        </div>
-    </div>
+        </Box>
+        <Box>
+            <Text mb="1rem">
+                A <Text color="#ff9140">hackathon</Text> 
+                is a social coding event where <Text color="#46daae">teens come together</Text> 
+                to <Text color="#ed445c">build projects</Text> 
+                in a short amount of time and <Text color="#62c3df">share them with the world</Text>.
+            </Text>
+        </Box>
+    </Box>
     );
 };
 
