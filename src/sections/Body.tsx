f76833f91fb7773a3c1b9b2828099f3fd1a69ad3
ry-se <ryans1633@gmail.com>
2022-12-03 14:17:00 +0800

    adding gridItems

--- src/sections/Body.tsx
+++ src/sections/Body.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, Grid, GridItem} from "@chakra-ui/react";
 import { motion } from 'framer-motion';
 import Section from "../components/Section";
 
@@ -14,9 +14,18 @@ const Body: React.FC = () => {
         <Box p="5rem">
             <Text as="b" fontSize="3rem" color="#FFFFFF">
                 A <Text as="b" color="#ff9140">hackathon</Text> is a social coding event where <Text as="b" color="#46daae">teens come together </Text> 
-                to <Text as="b" color="#ed445c">build projects</Text> in a short amount of time and <Text as="b" color="#62c3df">share them with the world</Text>.
+                to <Text as="b" color="#ed445c">build projects</Text> in a short amount of time and <Text color="#62c3df">share them with the world.</Text>
+                At Lion City Hacks, 100+ teenagers will gather to:
             </Text>
         </Box>
+        <Grid templateColumns='repeat(2, 1fr)' gap={6}>
+            <GridItem w='100%' h='10' bg='blue.500' />
+            <GridItem w='100%' h='10' bg='blue.500' />
+            <GridItem w='100%' h='10' bg='blue.500' />
+            <GridItem w='100%' h='10' bg='blue.500' />
+            <GridItem w='100%' h='10' bg='blue.500' />
+            <GridItem w='100%' h='10' bg='blue.500' />
+        </Grid>
     </Section>
     );
 };
